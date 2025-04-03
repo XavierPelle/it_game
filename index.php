e9801003de21684e702699b8adae4ed6b958ceb2
Presley OBIANG <obiangabaghapresley@gmail.com>
2025-04-03 14:40:44 +0200

    Sauvegarde

--- index.php
+++ index.php
@@ -10,12 +10,22 @@ $logsDecoderController = new LogsDecoderController();
 
 $router->addRoute('GET', '/fetch-logs', [$fetchLogService, 'fetchLogFile']);
 $router->addRoute('GET', '/decode-logs', [$logsDecoderController, 'decodeFile']);
-$router->addRoute('GET', '/analyze', [$logsDecoderController, 'analyzeLogs']);
+$router->addRoute('GET', '/analyze', [$logsDecoderController, 'decodeFile']);
 $router->addRoute('GET', '/read', [$logsDecoderController, 'readDecodedFile']);
 $router->addRoute('GET', '/analyzeIP', [$logsDecoderController, 'analyzeIpWithVirusTotal']);
 $router->addRoute('GET', '/analyzeURL', [$logsDecoderController, 'analyzeUrlWithVirusTotal']);
 $router->addRoute('GET', '/dashboard', [$logsDecoderController, 'showDashboard']);
 $router->addRoute('GET', '/test', [$logsDecoderController, 'test']);
+$router->addRoute('POST', '/upload', [$fetchLogService, 'uploadLogFile']);
+
+
+
+
+
+
+
+
+
 
 
 $router->handleRequest();
